9407b8700cdf00e8e354201acf615b088d86f5ec
David Stangl <david.stangl@student.hpi.de>
2016-05-02 23:26:09 +0200

    Added cli interface

--- src/main/kotlin/io/github/codeocean_scrapper/Main.kt
+++ src/main/kotlin/io/github/codeocean_scrapper/Main.kt
@@ -2,19 +2,29 @@ package io.github.codeocean_scrapper
 
 import io.github.codeocean_scrapper.analysis.analyseAndSave
 import io.github.codeocean_scrapper.analysis.findStudentSubmissionPages
+import java.net.URL
 import java.nio.file.Paths
 
-private val baseUrl = "https://codeocean.openhpi.de"
-private val statUrl = "https://codeocean.openhpi.de/exercises/154/statistics"
-private val sessionKey = "bmJOT1FPLzcwWG84WDdBcmM1QkNTVDloYnQ4NTJ4bWdqVFZhelhtTjlkSEJ4NEtnMzRsWEVMOERxdVR2OStCcXIxOHVwNW1lY0JiNE1nSWFPdEEzL1ljRnRocFdUS05sbUY4bWhqK2wzanlmRi9pYVNDZmZYVk5vOTZXWkpvNkdWSmRmUWY2V2lwaW5sclhVODRaNDVmekNUTlJabTdhSWQ1T1JNcHIyL0lzPS0tQVAzcFd4eHFIU21MdDNOT2sveCs2Zz09--7b8494ea3cdca414dea39239654a44597ce7a93b"
-private val targetDirectory = Paths.get("/Users/david/Desktop/Pt2Submissions")
+
+fun printUsage() {
+    println("Usage: codeocean_scrapper targetDirectory statisticsUrl sessionKey")
+    System.exit(1)
+}
 
 fun main(args: Array<String>) {
+    if (args.size != 3) {
+        printUsage()
+    }
+
+    val (targetDirectory, statsUrlString, sessionKey) = args
+    val statsUrl = URL(statsUrlString)
+    val baseUrl = "${statsUrl.protocol}://${statsUrl.authority}"
+
     val fetcher = PageFetcher(sessionKey)
-    val statDoc = fetcher.fetch(statUrl)
+    val statDoc = fetcher.fetch(statsUrlString)
     val studentSubmissionPages = findStudentSubmissionPages(statDoc, baseUrl)
     for ((name, url) in studentSubmissionPages) {
-        analyseAndSave(url, fetcher, name, targetDirectory)
+        analyseAndSave(url, fetcher, name, Paths.get(targetDirectory))
     }
 }
 
