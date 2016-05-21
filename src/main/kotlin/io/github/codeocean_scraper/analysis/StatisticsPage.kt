c10987b93f1e7dc4d2065e6078ae0a9c1b3afb5b
Tim Neumann <mail@timnn.me>
2016-05-21 16:44:50 +0200

    update selector for changed page structure

--- src/main/kotlin/io/github/codeocean_scraper/analysis/StatisticsPage.kt
+++ src/main/kotlin/io/github/codeocean_scraper/analysis/StatisticsPage.kt
@@ -6,7 +6,7 @@ fun findStudentSubmissionPages(
         doc: Document,
         baseUrl: String
 ): List<Pair<String, String>> {
-    val table = doc.select("strong:contains(External Users) + div > table").first()
+    val table = doc.select("strong:contains(External Users) ~ div > table").first()
     val links = table.select("tbody > tr > td > a")
     return links.map { it.text() to (baseUrl + it.attr("href")) }
 }
